a3d850fa54d0924ab06f0378347cfd88d3673624
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-11-06 08:33:18 +0800

    Update ipq807x-diy-part1.sh

--- ipq807x-diy-part1.sh
+++ ipq807x-diy-part1.sh
@@ -14,8 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-#echo 'src-git Boos4721 https://github.com/Boos4721/OpenWrt-Packages' >>feeds.conf.default
-#echo 'src-git fichenx https://github.com/fichenx/openwrt-package' >>feeds.conf.default
+#openclash
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+#主题列表
+echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
