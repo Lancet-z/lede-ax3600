6b85f8f822655512796dacd5221bb9cc8198563f
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-11-22 08:28:38 +0800

    Update ipq807x-diy-part1.sh

--- ipq807x-diy-part1.sh
+++ ipq807x-diy-part1.sh
@@ -14,13 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-rm -rf feeds/luci/themesluci-theme-argon
-rm -rf feeds/luci/applications/luci-app-netdata
+
 #openclash
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 #主题列表
 echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
-#netdata汉化版
-echo "src-git cups https://github.com/sirpdboy/luci-app-netdata" >> feeds.conf.default
 #argon主题
 echo "src-git jerryk https://github.com/jerrykuku/openwrt-package" >> feeds.conf.default
