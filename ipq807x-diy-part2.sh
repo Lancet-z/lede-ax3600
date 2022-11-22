11fd21aaa4dc6903413e0729e2c873d44fa6c6ec
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-11-22 08:01:21 +0800

    Update ipq807x-diy-part2.sh

--- ipq807x-diy-part2.sh
+++ ipq807x-diy-part2.sh
@@ -10,12 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-#添加主题
-# Add luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
-rm -rf ../lean/luci-theme-argon
-
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
